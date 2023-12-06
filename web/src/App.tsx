f31c33fe86f498b40fe422250b2544fa125f0312
sajithaliyanage <sajithaliyanage@gmail.com>
2023-12-06 16:09:08 +0300

    Add RTL support using stylis-rtl-plugin

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,32 @@
 import { lazy, Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { prefixer } from 'stylis';
+import rtlPlugin from 'stylis-plugin-rtl';
 import CssBaseline from '@mui/material/CssBaseline';
-import { PaletteOptions, Theme, ThemeProvider, createTheme } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import createCache, { EmotionCache } from '@emotion/cache';
+import { Direction, PaletteOptions, Theme, ThemeProvider, createTheme } from '@mui/material/styles';
 
 import Layout from './components/Layout';
 import LazyProgress from './components/LazyProgress';
 import { RootState } from './store/store';
+import { DIRECTIONS } from './utils/common';
 
 const News = lazy(() => import('./pages/News'));
 
 const App = () => {
   const palette: PaletteOptions = useSelector((state: RootState) => state.theme.palette);
+  const isRTL: boolean = useSelector((state: RootState) => state.theme.isRTL);
 
   const appTheme: Theme = createTheme({
     palette,
+    direction: isRTL ? (DIRECTIONS.RTL_MODE as Direction) : (DIRECTIONS.LTR_MODE as Direction),
+  });
+
+  const cacheRTL: EmotionCache = createCache({
+    key: 'muirtl',
+    stylisPlugins: isRTL ? [prefixer, rtlPlugin] : [prefixer],
   });
 
   const routes = (
@@ -25,12 +37,16 @@ const App = () => {
   );
 
   return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline />
-      <Layout>
-        <Suspense fallback={<LazyProgress />}>{routes}</Suspense>
-      </Layout>
-    </ThemeProvider>
+    <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={appTheme}>
+        <div dir={isRTL ? DIRECTIONS.RTL_MODE : DIRECTIONS.LTR_MODE}>
+          <CssBaseline />
+          <Layout>
+            <Suspense fallback={<LazyProgress />}>{routes}</Suspense>
+          </Layout>
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
 };
 
--- web/src/utils/common.ts
+++ web/src/utils/common.ts
@@ -20,6 +20,11 @@ export const NEWS_FILTER: NewsFilter = {
     ],
 };
 
+export const DIRECTIONS: { LTR_MODE: string, RTL_MODE: string } = {
+    LTR_MODE: 'ltr',
+    RTL_MODE: 'rtl'
+}
+
 export const toProperCase = (text: string): string => {
     return text.replace(/\w\S*/g, function (matchedText) {
         return matchedText.charAt(0).toUpperCase() + matchedText.substr(1).toLowerCase();
